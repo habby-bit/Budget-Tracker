5b8cd7d79aae0cdb9b835c450540a6de386a3359
Habby Olusesi <habbyolu@gmail.com>
2021-01-20 11:48:34 -0500

    created indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
+  "/db.js",
   "/index.html",
   "/index.js",
   "/manifest.webmanifest",
