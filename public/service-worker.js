8b29c1db4876e49ce9de3dbf5a24c3d49c3492b2
Habby Olusesi <habbyolu@gmail.com>
2021-01-20 11:51:57 -0500

    cleaned comments

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,24 +14,18 @@ const FILES_TO_CACHE = [
   "/images/BudgetStill.png"
 ];
 
-// install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
   evt.waitUntil(
     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
   );
     
-  // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -49,14 +43,12 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -64,7 +56,6 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
